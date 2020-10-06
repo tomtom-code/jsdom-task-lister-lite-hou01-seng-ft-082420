1e984b0b213a983316a418567a2ed3fea9321e47
tomtom-code <tomlai8882000@hotmail.com>
2020-10-06 17:59:15 -0500

    able to put up task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //console.log('We be loaded')
+  let form = document.querySelector('#create-task-form')
+  //console.log('this is my form,', form)
+  form.addEventListener('submit', function(e){
+    //console.log(e)
+    e.preventDefault();
+    const newTask = document.querySelector('#new-task-description').value
+    //console.log(newTask)
+
+
+    let li = document.createElement('li')
+    li.innerText = newTask
+    let tasks = document.querySelector('#tasks')
+    tasks.append(li)
+
+    
+
+  })
+   
 });
