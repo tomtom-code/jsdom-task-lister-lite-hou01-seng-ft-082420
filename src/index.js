1640e1f1c5ffab1724b31fc2b8ab3b435ecdd6fe
tomtom-code <tomlai8882000@hotmail.com>
2020-10-07 23:40:01 -0500

    finsih the basic part

--- src/index.js
+++ src/index.js
@@ -4,16 +4,39 @@ document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector('#create-task-form')
   //console.log('this is my form,', form)
   form.addEventListener('submit', function(e){
-    //console.log(e)
+    
     e.preventDefault();
     const newTask = document.querySelector('#new-task-description').value
     //console.log(newTask)
 
+  
 
+    //create post the new task
     let li = document.createElement('li')
     li.innerText = newTask
+    
+    //create button
+    let button = document.createElement('button')
+    button.innerText = 'x'
+    li.append(button)
+    
+    //put that (newtask + x button) to the ul-tasks 
     let tasks = document.querySelector('#tasks')
     tasks.append(li)
+
+
+    // give the button the function to remove the li-new task
+    button.addEventListener('click', function(){
+      li.remove()
+
+    });
+    form.reset()
+
+    
+
+
+
+    
 
     
 
